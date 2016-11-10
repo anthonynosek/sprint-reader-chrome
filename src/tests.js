ba1f3aef607a150889b2f9572c63fea75fceb27b
twtu <nazhatson@gmail.com>
2016-11-10 12:18:39 -0500

    Typo in test description

--- src/tests.js
+++ src/tests.js
@@ -6,5 +6,5 @@ QUnit.test( "engine.js tests", function( assert ) {
 	madvEnableSpaceInsertion = 'true';
 	splitTextToArray_FirstPass("Wordone.Wordtwo", 1);
 	assert.strictEqual(splitTextFirstPass[0].text, "Wordone.", "Tested 'Wordone.Wordtwo' is split into two when madvEnableSpaceInsertion (word 1)");
-	assert.strictEqual(splitTextFirstPass[1].text, "Wordtwo", "Tested 'Wordone.Wordtwo' is split into two when madvEnableSpaceInsertion (word 1)");
+	assert.strictEqual(splitTextFirstPass[1].text, "Wordtwo", "Tested 'Wordone.Wordtwo' is split into two when madvEnableSpaceInsertion (word 2)");
 });
